af0a8dfa25125eea08ae98376e942b664f3aaceb
sokairavi <soma.creativeonline@gmail.com>
2022-11-30 21:09:25 -0500

    homepage

--- a-little-pepper-front-end/src/Components/Homepage.js
+++ a-little-pepper-front-end/src/Components/Homepage.js
@@ -2,42 +2,37 @@ import { Row, Form, Button } from "react-bootstrap";
 
 import { useState } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 
-const ACCESS_POINT = process.env.ACCESS_POINT;
-const API_KEY = process.env.API_KEY;
+import Recipe from "./Recipe";
+
+// const ACCESS_POINT = process.env.ACCESS_POINT;
+// const API_KEY = process.env.API_KEY;
 
 export default function Homepage() {
 
-    const [searchRecipe, setSearchRecipe] = useState({});
-    const [input, setInput] = useState('');
-    const [recipes, setRecipes] = useState({
-        name: '',
-        cal: 0,
-        fat: 0,
-        carb: 0,
-        protein: 0,
-        photos: '',
-        ingredients: [],
-        instructions: [],
-    });
+    const [recipes, setRecipes] = useState([]);
+    const [input1, setInput1] = useState('');
+ 
 
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
 
     const getRecipes = () => {
-        // axios.get(`${ACCESS_POINT}/findByIngredients?apiKey=${API_KEY}&ingredients=${input},`)
-        axios.get(`https://api.spoonacular.com/recipes/findByIngredients?apiKey=df49e55434e34cab8f10d2a1cad00bee&ingredients=${input}`)
-            .then(res => setSearchRecipe(res.data))
+        // axios.get(`${ACCESS_POINT}/findByIngredients?apiKey=${API_KEY}&ingredients=${input},${input2},${input3}`)
+        axios.get(`https://api.spoonacular.com/recipes/findByIngredients?apiKey=df49e55434e34cab8f10d2a1cad00bee&ingredients=${input1}`)
+            .then(res => setRecipes(res.data))
             .catch(error => console.error(error))
     }
 
+    // const name = searchRecipe.name
+
     const handleSubmit = (event) => {
         event.preventDefault();
-        setInput(input);
+        setInput1(input1);
         getRecipes();
     };
 
-    console.log(searchRecipe)
+    console.log(recipes)
 
     return (
         <>
@@ -50,27 +45,32 @@ export default function Homepage() {
                     >
                         <div className="content mx-auto">
                             <Row className="g-6">
-                                <Form.Label>Ingredient 1</Form.Label>
+                                <Form.Label>Add Ingredints Seperated by Commas</Form.Label>
                                 <Form.Control
                                     className="mb-3"
                                     size="md"
                                     type="text"
-                                    onChange={(e) => setInput(e.target.value)}
-                                    value={input}
+                                    onChange={(e) => setInput1(e.target.value)}
+                                    value={input1}
+                                    placeholder="example: chicken,flour,salt"
                                     required
                                 />
-                                <Form.Label>Ingredient 2</Form.Label>
+                                {/* <Form.Label>Ingredient 2</Form.Label>
                                 <Form.Control
                                     className="mb-3"
                                     size="md"
                                     type="text"
+                                    onChange={(e) => setInput2(e.target.value)}
+                                    value={input2}
                                 />
                                 <Form.Label>Ingredient 3</Form.Label>
                                 <Form.Control
                                     className="mb-3"
                                     size="md"
                                     type="text"
-                                />
+                                    onChange={(e) => setInput3(e.target.value)}
+                                    value={input3}
+                                /> */}
                                 <div className=" mb-5">
                                     <Button
                                         type="submit"
@@ -87,7 +87,15 @@ export default function Homepage() {
                 </div>
             </Form>
             <article>
-           
+                <Row xs={1} md={2} lg={3} className="g-5 py-5">
+                    {recipes.map((recipe) => {
+                        return (
+                            <>
+                                <Recipe recipe={recipe} />
+                            </>
+                        )
+                    })}
+                </Row>
             </article>
         </>
     )
