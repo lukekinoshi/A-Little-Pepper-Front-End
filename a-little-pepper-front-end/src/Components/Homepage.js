8c7a5d852077675fe1372cc41d4516ee8044508e
Khaleed Uddin <uddin1786@gmail.com>
2022-11-30 21:18:44 -0500

    RecipeList.js is now Recipes.js, and we added recipe cards

--- a-little-pepper-front-end/src/Components/Homepage.js
+++ a-little-pepper-front-end/src/Components/Homepage.js
@@ -3,35 +3,23 @@ import { Row, Form, Button } from "react-bootstrap";
 import { useState } from "react";
 import axios from "axios";
 // import { useNavigate } from "react-router-dom";
-
-import RecipeList from "./RecipeList";
+import Recipe from "./Recipe";
 
 // const ACCESS_POINT = process.env.ACCESS_POINT;
 // const API_KEY = process.env.API_KEY;
 
 export default function Homepage() {
 
-    const [searchRecipe, setSearchRecipe] = useState([]);
+    const [recipes, setRecipes] = useState([]);
     const [input1, setInput1] = useState('');
-    const [input2, setInput2] = useState('');
-    const [input3, setInput3] = useState('');
-    // const [recipes, setRecipes] = useState({
-    //     name: '',
-    //     cal: 0,
-    //     fat: 0,
-    //     carb: 0,
-    //     protein: 0,
-    //     photos: '',
-    //     ingredients: [],
-    //     instructions: [],
-    // });
+  
 
     // const navigate = useNavigate();
 
     const getRecipes = () => {
         // axios.get(`${ACCESS_POINT}/findByIngredients?apiKey=${API_KEY}&ingredients=${input},${input2},${input3}`)
-        axios.get(`https://api.spoonacular.com/recipes/findByIngredients?apiKey=df49e55434e34cab8f10d2a1cad00bee&ingredients=${input1},${input2},${input3}`)
-            .then(res => setSearchRecipe(res.data))
+        axios.get(`https://api.spoonacular.com/recipes/findByIngredients?apiKey=df49e55434e34cab8f10d2a1cad00bee&ingredients=${input1}`)
+            .then(res => setRecipes(res.data))
             .catch(error => console.error(error))
     }
 
@@ -40,12 +28,10 @@ export default function Homepage() {
     const handleSubmit = (event) => {
         event.preventDefault();
         setInput1(input1);
-        setInput2(input2);
-        setInput3(input3);
         getRecipes();
     };
 
-    console.log(searchRecipe)
+    console.log(recipes)
 
     return (
         <>
@@ -58,16 +44,17 @@ export default function Homepage() {
                     >
                         <div className="content mx-auto">
                             <Row className="g-6">
-                                <Form.Label>Ingredient 1</Form.Label>
+                                <Form.Label>Add Ingredints Seperated by Commas</Form.Label>
                                 <Form.Control
                                     className="mb-3"
                                     size="md"
                                     type="text"
                                     onChange={(e) => setInput1(e.target.value)}
                                     value={input1}
+                                    placeholder="example: chicken,flour,salt"
                                     required
                                 />
-                                <Form.Label>Ingredient 2</Form.Label>
+                                {/* <Form.Label>Ingredient 2</Form.Label>
                                 <Form.Control
                                     className="mb-3"
                                     size="md"
@@ -82,7 +69,7 @@ export default function Homepage() {
                                     type="text"
                                     onChange={(e) => setInput3(e.target.value)}
                                     value={input3}
-                                />
+                                /> */}
                                 <div className=" mb-5">
                                     <Button
                                         type="submit"
@@ -99,7 +86,15 @@ export default function Homepage() {
                 </div>
             </Form>
             <article>
-            <RecipeList recipes={searchRecipe}/>
+                <Row xs={1} md={2} lg={3} className="g-5 py-5">
+                    {recipes.map((recipe) => {
+                        return (
+                            <>
+                                <Recipe recipe={recipe} />
+                            </>
+                        )
+                    })}
+                </Row>
             </article>
         </>
     )
--- a-little-pepper-front-end/src/Pages/Profile.js
+++ a-little-pepper-front-end/src/Pages/Profile.js
@@ -1,4 +1,4 @@
-import React from "react";
+import axios from "axios";
 import { UserAuth } from "../Context/AuthContext";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
@@ -8,6 +8,14 @@ export default function Profile() {
   const navigate = useNavigate();
   const { user, logOut } = UserAuth();
   const [profile, setProfile] = useState([]);
+  const API = process.env.REACT_APP_API_URL;
+
+  
+  useEffect(() => {
+    axios.get(`${API}/profiles/${user.uid}`).then((response) => {
+      setProfile(response.data);
+    });
+  }, []);
 
   const handleSignOut = async () => {
     try {
@@ -18,46 +26,49 @@ export default function Profile() {
     }
   };
 
+  const editProfile = () => {
+    navigate(`/`);
+  };
+
   return (
-    <div className="my-5" style={{ color: "white" }}>
+    <div className="my-5" style={{ color: "black" }}>
       <h2>Welcome {user.displayName} </h2>
+      <h3>{user.uid}</h3>
+      <div>
+        <Card style={{ backgroundColor: "orange", alignItems: "center" }}>
+          <Card.Img
+            variant="top"
+            // src={profile.picture}
+            // style={{ width: "300px" ,borderRadius: "50%"}}
+          />
+          <Card.Body>
+            {/* <Card.Title>{profile.name}</Card.Title> */}
+            <Card.Title>Profile</Card.Title>
+            <Card.Subtitle className="mb = ">{user.displayName}</Card.Subtitle>
+            <Card.Text>profile.cal</Card.Text>
+            <Card.Text>profile.fat</Card.Text>
+            <Card.Text>profile.carb</Card.Text>
+            <Card.Text>profile.protein</Card.Text>
+          </Card.Body>
 
-      {profile.id ? (
-        <div>
-          <Card style={{ backgroundColor: "#202225", alignItems: "center" }}>
-            <Card.Img
-              variant="top"
-              // src={profile.picture}
-              // style={{ width: "300px" ,borderRadius: "50%"}}
-            />
-            <Card.Body>
-              {/* <Card.Title>{profile.name}</Card.Title> */}
-              <Card.Title>UserName</Card.Title>
-              <Card.Subtitle className="mb-2 text-muted">text</Card.Subtitle>
-              <Card.Text>about</Card.Text>
-            </Card.Body>
-
-            <Card.Body>
-              <Card.Text>What to put here?</Card.Text>
-            </Card.Body>
-          </Card>
-          <div
-            className="d-flex align-items-center justify-content-center"
-            style={{ gap: ".5rem" }}
-          >
-            <Button variant="outline-light">Edit Profile</Button>
-
-            <Button variant="danger">Delete Profile</Button>
-            <Button variant="outline-dark" onClick={handleSignOut}>
-              Sign Out
-            </Button>
-          </div>
+          <Card.Body>
+            <Card.Text>What to put here?</Card.Text>
+          </Card.Body>
+        </Card>
+        <div
+          className="d-flex align-items-center justify-content-center"
+          style={{ gap: ".5rem" }}
+        >
+          <Button variant="outline-light">Edit Profile</Button>
+          <Button variant="outline-dark" onClick={handleSignOut}>
+            Sign Out
+          </Button>
         </div>
-      ) : (
-        <Button className="mt-5" variant="warning">
-          Create Profile
-        </Button>
-      )}
+      </div>
+
+      <Button onClick={editProfile} className="mt-5" variant="warning">
+        Edit Profile
+      </Button>
     </div>
   );
 }
