62d0aacdb02a3c00adb241fdcae0952b7e192e0d
Khaleed Uddin <uddin1786@gmail.com>
2022-12-03 16:50:51 -0500

    new apikey, recipedetails updated

--- a-little-pepper-front-end/src/Components/RecipeDetails.js
+++ a-little-pepper-front-end/src/Components/RecipeDetails.js
@@ -5,45 +5,43 @@ import axios from "axios";
 
 export default function RecipeDetails() {
 
-  const [instructions, setInstructions] = useState([]);
   const [nutrition, setNutrition] = useState([]);
+  const [ingredients, setIngredients] = useState([]);
+  const [instructions, setInstructions] = useState([]);
 
   let { id } = useParams();
 
   // const navigate = useNavigate();
-
   useEffect(
     () => {
-      const getInstructions = () => {
-        // axios.get(`${ACCESS_POINT}/findByIngredients?apiKey=${API_KEY}&ingredients=${input1}`)
-        axios.get(`https://api.spoonacular.com/recipes/${id}/analyzedInstructions?apiKey=2a7afb1b7ce041f181cc367985fd5f7b`)
-          .then(res => setInstructions(res.data))
-          .catch(error => console.error(error))
-      }
-    }
-  );
+      // axios.get(`${ACCESS_POINT}/findByIngredients?apiKey=${API_KEY}&ingredients=${input1}`)
+      axios.get(`https://api.spoonacular.com/recipes/${id}/analyzedInstructions?apiKey=9ab6abef58ff4088ab12f31009f1a8a9`)
+        .then(res => setInstructions(res.data))
+        .catch(error => console.error(error))
 
-  useEffect(
-    () => {
-      const getNutrition = () => {
-        // axios.get(`${ACCESS_POINT}/findByIngredients?apiKey=${API_KEY}&ingredients=${input1}`)
-        axios.get(`https://api.spoonacular.com/recipes/${id}/nutritionWidget.json?apiKey=2a7afb1b7ce041f181cc367985fd5f7b`)
-          .then(res => setNutrition(res.data))
-          .catch(error => console.error(error))
-      }
-    }
-  );
-  // getInstructions();
-  // getNutrition();
-  //^^ these two invokes runs the axios fetch but it there is no way to stop it so i keeps sending requests and have used my the daily quota
+        axios.get(`https://api.spoonacular.com/recipes/${id}/nutritionWidget.json?apiKey=9ab6abef58ff4088ab12f31009f1a8a9`)
+        .then(res => setNutrition(res.data))
+        .catch(error => console.error(error))
+
+        axios.get(`https://api.spoonacular.com/recipes/${id}/ingredientWidget.json?apiKey=9ab6abef58ff4088ab12f31009f1a8a9`)
+        .then(res => setIngredients(res.data))
+        .catch(error => console.error(error))
+    }, []);
 
 
-  // console.log(instructions)
-  // console.log(nutrition)
+
+    console.log(nutrition)
+    console.log(ingredients)
+    console.log(instructions[0])
 
   return (
     <article className='RecipeDetails'>
       <h1>{id}</h1>
+      <h1>{nutrition.calories}ilo calories</h1>
+      <h6>*Note: 1000kilo calories = 1 calorie</h6>
+      <h1>Fat: {nutrition.fat}</h1>
+      <h1>Carbohydrates: {nutrition.carbs}</h1>
+      <h1>Protein: {nutrition.protein}</h1>
     </article>
   )
 }
