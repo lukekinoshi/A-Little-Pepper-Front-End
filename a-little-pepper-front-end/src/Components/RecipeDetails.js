2d8ed40c60784ee51bf2544f1ca380ad6ad8b7ec
Khaleed Uddin <uddin1786@gmail.com>
2022-12-03 15:23:43 -0500

    useeffect in recipedetails

--- a-little-pepper-front-end/src/Components/RecipeDetails.js
+++ a-little-pepper-front-end/src/Components/RecipeDetails.js
@@ -1,5 +1,5 @@
 import { useParams, useNavigate } from "react-router-dom"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import axios from "axios";
 
 
@@ -12,19 +12,27 @@ export default function RecipeDetails() {
 
   // const navigate = useNavigate();
 
-  const getInstructions = () => {
-    // axios.get(`${ACCESS_POINT}/findByIngredients?apiKey=${API_KEY}&ingredients=${input1}`)
-    axios.get(`https://api.spoonacular.com/recipes/${id}/analyzedInstructions?apiKey=2a7afb1b7ce041f181cc367985fd5f7b`)
-      .then(res => setInstructions(res.data))
-      .catch(error => console.error(error))
-  }
+  useEffect(
+    () => {
+      const getInstructions = () => {
+        // axios.get(`${ACCESS_POINT}/findByIngredients?apiKey=${API_KEY}&ingredients=${input1}`)
+        axios.get(`https://api.spoonacular.com/recipes/${id}/analyzedInstructions?apiKey=2a7afb1b7ce041f181cc367985fd5f7b`)
+          .then(res => setInstructions(res.data))
+          .catch(error => console.error(error))
+      }
+    }
+  );
 
-  const getNutrition = () => {
-    // axios.get(`${ACCESS_POINT}/findByIngredients?apiKey=${API_KEY}&ingredients=${input1}`)
-    axios.get(`https://api.spoonacular.com/recipes/${id}/nutritionWidget.json?apiKey=2a7afb1b7ce041f181cc367985fd5f7b`)
-      .then(res => setNutrition(res.data))
-      .catch(error => console.error(error))
-  }
+  useEffect(
+    () => {
+      const getNutrition = () => {
+        // axios.get(`${ACCESS_POINT}/findByIngredients?apiKey=${API_KEY}&ingredients=${input1}`)
+        axios.get(`https://api.spoonacular.com/recipes/${id}/nutritionWidget.json?apiKey=2a7afb1b7ce041f181cc367985fd5f7b`)
+          .then(res => setNutrition(res.data))
+          .catch(error => console.error(error))
+      }
+    }
+  );
   // getInstructions();
   // getNutrition();
   //^^ these two invokes runs the axios fetch but it there is no way to stop it so i keeps sending requests and have used my the daily quota
