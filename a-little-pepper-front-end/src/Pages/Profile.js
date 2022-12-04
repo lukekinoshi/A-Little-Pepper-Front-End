18ea0bc5aebc8cb88c975ee03a4a292bcd79849c
Mason Mei <masonmei@pursuit.org>
2022-12-04 15:26:07 -0500

    updated profile

--- a-little-pepper-front-end/src/Pages/Profile.js
+++ a-little-pepper-front-end/src/Pages/Profile.js
@@ -12,13 +12,14 @@ export default function Profile() {
   const [profile, setProfile] = useState([]);
   const [savedRecipes, setSavedRecipes] = useState([]);
   const API = process.env.REACT_APP_API_URL;
+  const [create, setCreate] = useState(false)
 
   useEffect(() => {
     axios.get(`${API}/profiles/${user.uid}`).then((response) => {
       setProfile(response.data);
       setSavedRecipes(response.data.recipes);
     });
-  }, []);
+  }, [create]);
 
   const handleSignOut = async () => {
     try {
@@ -29,38 +30,54 @@ export default function Profile() {
     }
   };
 
+  const createProfile = () => {
+    axios.post(`${API}/profiles`, {"uid": user.uid,
+    "name": user.displayName,
+    "cal": 0,
+    "fat": 0,
+    "carb": 0,
+    "protein": 0,
+    "recipes": []})
+    .then(() => {
+      setCreate(!create)
+      console.log("post sent")
+    })
+  }
+
   return (
     <div className="my-5" style={{ color: "black" }}>
-      <article className="mb-5">
-        <CalorieTracker />
-      </article>
-      <div>
-        <Card style={{ backgroundColor: "orange", alignItems: "center" }}>
-          <Card.Img
-            variant="top"
-            // src={profile.picture}
-            // style={{ width: "300px" ,borderRadius: "50%"}}
-          />
-          <Card.Body>
-            {/* <h1>lol</h1> */}
-            <Card.Title>{user.displayName}</Card.Title>
-            <Card.Text>Calories: {profile.cal}</Card.Text>
-            <Card.Text>Fat: {profile.fat}</Card.Text>
-            <Card.Text>Carbs: {profile.carb}</Card.Text>
-            <Card.Text>Protein: {profile.protein}</Card.Text>
-          </Card.Body>
-        </Card>
-        <div
-          className="d-flex align-items-center justify-content-center"
-          style={{ gap: ".5rem" }}
-        >
-          <Button variant="light" onClick={handleSignOut}>
-            Sign Out
-          </Button>
-        </div>
-        <section className="my-5">
-          <h2>Bookmarked Recipes</h2>
-          <Row xs={1} md={2} lg={3} className="g-5 py-5">
+      {profile.id ? (
+        <>
+          <article className="mb-5">
+            <CalorieTracker />
+          </article>
+          <div>
+            <Card style={{ backgroundColor: "orange", alignItems: "center" }}>
+              <Card.Img
+                variant="top"
+                // src={profile.picture}
+                // style={{ width: "300px" ,borderRadius: "50%"}}
+              />
+              <Card.Body>
+                {/* <h1>lol</h1> */}
+                <Card.Title>{user.displayName}</Card.Title>
+                <Card.Text>Calories: {profile.cal}</Card.Text>
+                <Card.Text>Fat: {profile.fat}</Card.Text>
+                <Card.Text>Carbs: {profile.carb}</Card.Text>
+                <Card.Text>Protein: {profile.protein}</Card.Text>
+              </Card.Body>
+            </Card>
+            <div
+              className="d-flex align-items-center justify-content-center"
+              style={{ gap: ".5rem" }}
+            >
+              <Button variant="light" onClick={handleSignOut}>
+                Sign Out
+              </Button>
+            </div>
+            <section className="my-5">
+              <h2>Bookmarked Recipes</h2>
+              {/* <Row xs={1} md={2} lg={3} className="g-5 py-5">
             {savedRecipes.map((recipe) => {
               return (
                 <>
@@ -68,9 +85,15 @@ export default function Profile() {
                 </>
               );
             })}
-          </Row>
-        </section>
-      </div>
+          </Row> */}
+            </section>
+          </div>
+        </>
+      ) : (
+        <Button onClick={createProfile} className="mb-5" variant="primary">
+          Create Profile
+        </Button>
+      )}
     </div>
   );
 }
