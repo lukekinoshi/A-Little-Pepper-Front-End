dfa223431a5aad9e4b9c658835bc605be83a032e
Mason Mei <masonmei@pursuit.org>
2022-12-09 23:13:28 -0500

    added about component/styled home

--- a-little-pepper-front-end/src/Pages/Home.js
+++ a-little-pepper-front-end/src/Pages/Home.js
@@ -2,7 +2,7 @@ import { Row, Form, Button } from "react-bootstrap";
 import pepper from "../Assets/chili-pepper.png";
 import { useState } from "react";
 import axios from "axios";
-import About from '../Components/About'
+import About from "../Components/About";
 import padThai from "../Assets/pad-thai.png";
 // import { useNavigate } from "react-router-dom";
 
@@ -39,29 +39,30 @@ export default function Homepage() {
 
   return (
     <div className="my-5">
-      <img src={padThai} width="50%" />
-      <h2>Search for a recipe</h2>
-      <Form onSubmit={handleSubmit}>
-        <div className="row">
-          <main
-            className="col-lg-12 mx-auto py-3"
-            style={{ maxWidth: "350px" }}
-          >
-            <div className="content mx-auto">
-              <Row className="g-6">
-                <Form.Label>
-                  Enter your ingredients seperated by commas
-                </Form.Label>
-                <Form.Control
-                  className="mb-3"
-                  size="md"
-                  type="text"
-                  onChange={(e) => setInput1(e.target.value)}
-                  value={input1}
-                  placeholder="example: chicken,flour,salt"
-                  required
-                />
-                {/* <Form.Label>Ingredient 2</Form.Label>
+      <div>
+        <img src={padThai} width="50%" />
+        <h2 className="mt-4">Search for a recipe</h2>
+        <Form onSubmit={handleSubmit}>
+          <div className="row">
+            <main
+              className="col-lg-12 mx-auto py-3"
+              style={{ maxWidth: "350px" }}
+            >
+              <div className="content mx-auto">
+                <Row className="g-6">
+                  <Form.Label>
+                    Enter your ingredients seperated by commas
+                  </Form.Label>
+                  <Form.Control
+                    className="mb-3"
+                    size="lg"
+                    type="text"
+                    onChange={(e) => setInput1(e.target.value)}
+                    value={input1}
+                    placeholder="example: chicken,flour,salt"
+                    required
+                  />
+                  {/* <Form.Label>Ingredient 2</Form.Label>
                                 <Form.Control
                                     className="mb-3"
                                     size="md"
@@ -79,27 +80,28 @@ export default function Homepage() {
 
                                 /> */}
 
-                <div className=" mb-5">
-                  <Button
-                    type="submit"
-                    style={{ maxWidth: "200px" }}
-                    variant="danger"
-                    size="md"
-                  >
-                    Let's Get Spicy
-                    <img
-                      style={{ marginLeft: "5px" }}
-                      src={pepper}
-                      width="25"
-                      height="25"
-                    />
-                  </Button>
-                </div>
-              </Row>
-            </div>
-          </main>
-        </div>
-      </Form>
+                  <div className=" mb-5">
+                    <Button
+                      type="submit"
+                      style={{ maxWidth: "200px" }}
+                      variant="danger"
+                      size="lg"
+                    >
+                      Let's Get Spicy
+                      <img
+                        style={{ marginLeft: "5px" }}
+                        src={pepper}
+                        width="25"
+                        height="25"
+                      />
+                    </Button>
+                  </div>
+                </Row>
+              </div>
+            </main>
+          </div>
+        </Form>
+      </div>
       <article>
         <Row xs={1} md={2} lg={3} className="g-5 py-5">
           {recipes.map((recipe) => {
@@ -111,7 +113,7 @@ export default function Homepage() {
           })}
         </Row>
       </article>
-      <About/>
+      <About />
     </div>
   );
 }
